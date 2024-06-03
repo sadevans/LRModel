9ca7d360d9377a0d5427f5bb9b976c24f2ff840c
sadevans <a.s.gorokhova@yandex.ru>
2024-06-03 18:35:49 +0300

    chenges

--- NEW/model/frontend.py
+++ NEW/model/frontend.py
@@ -4,41 +4,6 @@ import yaml
 import numpy as np
 
 
-
-
-# class Conv3dResNet(torch.nn.Module):
-#     """Conv3dResNet module"""
-
-#     def __init__(self, backbone_type="resnet", relu_type="swish"):
-#         """__init__.
-
-#         :param backbone_type: str, the type of a visual front-end.
-#         :param relu_type: str, activation function used in an audio front-end.
-#         """
-#         super(Conv3dResNet, self).__init__()
-#         # self.run_mode = run_mode
-#         self.frontend_nout = 64
-#         self.trunk = ResNet(BasicBlock, [2, 2, 2, 2], relu_type=relu_type)
-#         self.frontend3D = nn.Sequential(
-#             nn.Conv3d(
-#                 1, self.frontend_nout, (5, 7, 7), (1, 2, 2), (2, 3, 3), bias=False
-#             ),
-#             nn.BatchNorm3d(self.frontend_nout),
-#             Swish(),
-#             nn.MaxPool3d((1, 3, 3), (1, 2, 2), (0, 1, 1)),
-#         )
-
-#     def forward(self, xs_pad):
-#         xs_pad = xs_pad.transpose(1, 2)  # [B, T, C, H, W] -> [B, C, T, H, W]
-
-#         B, C, T, H, W = xs_pad.size()
-#         xs_pad = self.frontend3D(xs_pad)
-#         Tnew = xs_pad.shape[2]
-#         xs_pad = threeD_to_2D_tensor(xs_pad)
-#         xs_pad = self.trunk(xs_pad)
-#         return xs_pad.view(B, Tnew, xs_pad.size(1))
-
-
 class Swish(nn.Module):
     """Construct an Swish object."""
 
